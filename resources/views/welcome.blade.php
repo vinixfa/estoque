ea2d8518bd8bfd3612aff2ed0cbf2a7bfd266a05
Vinícius Almeida <91106730+vinixfa@users.noreply.github.com>
2022-11-23 21:23:30 -0300

    iago

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     </head>
     <body class="antialiased">
 
-      
+    <!------- login----------------->
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             >>>>>>>>>>>>>
             @if (Route::has('login'))
@@ -33,15 +33,17 @@
                     @endauth
                 </div>
             @endif
-            >>>>>>>>>>>>>>
+            <!------- fim do login----------------->
+            <!------- imagem----------------->
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
                         <g clip-path="url(#clip0)" fill="#EF3B2D">
-                            <img src="{{asset('img/forum.jpg')}}" width="500%"> 
+                            <img src="{{asset('img/forum.jpg')}}" width="50%"> 
                     </svg>
                 </div>
-
+            <!-------fim imagem----------------->
+            <!-------.......----------->
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
@@ -49,13 +51,14 @@
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Forúm da Beleza</a></div>
                             </div>
-
+            <!------- login----------------->
                             <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                <div class="mt-2 text-yellow-600 dark:text-gray-400 text-sm">
                                     texto
                                 </div>
                             </div>
                         </div>
+            <!------- login----------------->
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
